86192a8dd76af11c52eba71bc9ea9006d76abe5f
Mario Susanto <msus0004@student.monash.edu>
2022-08-03 18:03:58 +0800

    designed app UI

--- src/main/App.java
+++ src/main/App.java
@@ -35,14 +35,25 @@ public class App extends JFrame {
     }
 
     private void start() {
-        Label title = new Label(this,"Insert title here",0,20,20,580,80);
-        title.setFont(new Font(Font.SERIF, Font.PLAIN,  40));
-        new Button(this,"push me",300,300,400,320);
-        new ComboBox<>(this,new String[] {"A","B","C"},200,200,280,220);
-        new TextField(this,"Welcome to Javatpoint.",50,100, 250,130);
-        new TextField(this,"AWT Tutorial",50,150, 250,180);
-        new ListObj<>(this,new String[] {"D", "E", "F","G","H","I"},400,400,480,460);
-        new TextArea(this, "yes yes",400,200,550,350);
+        Label lblTitle = new Label(this,"City Name Generator",0,20,20,580,80);
+        lblTitle.setFont(new Font(Font.SERIF, Font.PLAIN,  40));
+        Label lblPossible = new Label(this,"Possible Countries",0,20,120,280,180);
+        lblPossible.setFont(new Font(Font.SERIF, Font.PLAIN,  20));
+        ListObj<String> lstPossible = new ListObj<>(this,new String[] {"A", "B", "C","D","E","F"},80,180,220,400);
+
+        Label lblSelected = new Label(this,"Selected Countries",0,320,120,580,180);
+        lblSelected.setFont(new Font(Font.SERIF, Font.PLAIN,  20));
+        ListObj<String> lstSelected = new ListObj<>(this,new String[] {"D", "E", "F","G","H","I"},380,180,520,400);
+
+        Button btnSelect = new Button(this,">>>>>",250,250,350,270);
+        Button btnDeselect = new Button(this,"<<<<<",250,310,350,330);
+
+        Label lblCityNum = new Label(this,"Number of cities",Label.RIGHT,50,450,200,470);
+        TextField txtCityNum = new TextField(this,"10",220,450, 280,470);
+        Button btnGenerate = new Button(this,"Generate",180,500,280,520);
+
+        TextArea txaOutput = new TextArea(this, "",300,420,550,550);
+
         this.showApp();
     }
     private void showApp() {
